f1299ed39f43e58fbe273100507efc8e82aced3e
Serhat Sevki Dincer <jfcgauss@gmail.com>
2015-11-03 11:10:06 +0300

    1st commit

---
+++ sb.go
@@ -0,0 +1,64 @@
+//	Some string utility functions
+package sixb
+
+import "unsafe"
+
+// A quick & dirty Text-to-Int function for short text inputs.
+func Txt2int(s string) (x uint64) {
+	for i := 0; i < len(s); i++ {
+		x = x<<7 ^ x>>57
+		x += uint64(s[i] - ' ')
+	}
+	return x
+}
+
+//	Accepts 0..:, @..Z, a..z & maps it onto 6-bits. This is actually a bijection & inverse of Sb2an().
+func An2sb(x byte) byte {
+	x = 122 - x
+	if x > 133 || x < 26 {
+		return x
+	}
+	if x > 63 {
+		return x - 11
+	}
+	if x > 58 {
+		return x + 70
+	}
+	if x > 31 {
+		return x - 6
+	}
+	return x + 97
+}
+
+//	Accepts 6-bits & maps it onto 0..:, @..Z, a..z. This is actually a bijection & inverse of An2sb().
+func Sb2an(x byte) byte {
+	x = 122 - x
+	if x < 70 {
+		return x - 11
+	}
+	if x < 97 {
+		return x - 6
+	}
+	if x < 245 {
+		return x
+	}
+	if x < 250 {
+		return x + 70
+	}
+	return x + 97
+}
+
+type slice struct { // not worth importing reflect
+	Data     uintptr
+	Len, Cap int
+}
+
+//	Convert byte slice to int slice
+func Bs2is(x []byte) (y []uint64) {
+	s := (*slice)(unsafe.Pointer(&y))
+	t := (*slice)(unsafe.Pointer(&x))
+	s.Data = t.Data
+	s.Len = t.Len >> 3
+	s.Cap = s.Len
+	return
+}
---
+++ sb_test.go
@@ -0,0 +1,37 @@
+package sixb
+
+import (
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	for i := 0; i < 256; i++ {
+		c := byte(i)
+		if c != Sb2an(An2sb(c)) {
+			t.Fatal("inverses do not work", c)
+		}
+	}
+
+	l := "0:@Zaz"
+	for i := 0; i < 6; i += 2 {
+		for c := l[i]; c <= l[i+1]; c++ {
+			if An2sb(c) > 63 {
+				t.Fatal("domain error")
+			}
+		}
+	}
+
+	for c := byte(' '); c < '0'; c++ {
+		if An2sb(c) > 127 {
+			t.Fatal("domain error")
+		}
+	}
+}
+
+func Test2(t *testing.T) {
+	x := []byte("qwert12345")
+	y := Bs2is(x)
+	if len(y) != 1 || cap(y) != 1 || y[0] != 3689065420975077233 {
+		t.Fatal("Set error")
+	}
+}
